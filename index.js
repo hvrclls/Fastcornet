373f2300cf9a37d92937003777d38768d8c88250
Hever Calles <hevercalles@gmail.com>
2020-11-13 17:13:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const inquirer = require('inquire')
+
+
+// array of questions for user
+const questions = () => inquirer.questions ([
+    
+]);
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
