13a49703ec536daa4ce5ba77841561368f222281
Hever Calles <hevercalles@gmail.com>
2020-11-13 17:49:20 -0500

    questions

--- index.js
+++ index.js
@@ -4,7 +4,32 @@ const inquirer = require('inquire')
 
 // array of questions for user
 const questions = () => inquirer.questions ([
-    
+    {
+        type: 'input',
+        name: 'project-title',
+        message: 'What is the name of your project?'
+    },    
+    {
+        type: 'input',
+        name: 'project-description',
+        message: 'Provide description of your project.'
+    },    
+    {
+        type: 'list',
+        name: 'project-license',
+        message: 'What license are you using?',
+        options: ['Apache License 2.0", "BSD 3-Clause "New" or "Revised" license", "BSD 2-Clause "Simplified" or "FreeBSD" license", "GNU General Public License (GPL)", "GNU Library or "Lesser" General Public License (LGPL)", "MIT license", "Mozilla Public License 2.0", "Common Development and Distribution License", "Eclipse Public License version 2.0']
+    },   
+    {
+        type: 'input',
+        name: 'git-username',
+        message: 'Enter your GitHub Username.'
+    },  
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email.'
+    },   
 ]);
 
 // function to write README file
