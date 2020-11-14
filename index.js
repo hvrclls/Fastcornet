3bc1b79972d10df179ba0fe3964c38bd6ea3e50b
Hever Calles <hevercalles@gmail.com>
2020-11-13 19:20:59 -0500

    almost there

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const fs = require('fs')
-const inquirer = require('inquire')
+const inquirer = require('inquirer')
 
 
 // array of questions for user
-const questions = () => inquirer.questions ([
+const questions = () => inquirer.prompt ([
     {
         type: 'input',
-        name: 'project-title',
+        name: 'projectTitle',
         message: 'What is the name of your project?'
     },    
     {
         type: 'input',
-        name: 'project-description',
+        name: 'projectDescription',
         message: 'Provide description of your project.'
     },    
     {
         type: 'list',
-        name: 'project-license',
+        name: 'projectLicense',
         message: 'What license are you using?',
-        options: ['Apache License 2.0", "BSD 3-Clause "New" or "Revised" license", "BSD 2-Clause "Simplified" or "FreeBSD" license", "GNU General Public License (GPL)", "GNU Library or "Lesser" General Public License (LGPL)", "MIT license", "Mozilla Public License 2.0", "Common Development and Distribution License", "Eclipse Public License version 2.0']
+        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
     },   
     {
         type: 'input',
-        name: 'git-username',
+        name: 'gitUsername',
         message: 'Enter your GitHub Username.'
     },  
     {
@@ -30,16 +30,17 @@ const questions = () => inquirer.questions ([
         name: 'email',
         message: 'Enter your email.'
     },   
-]);
+])
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+//![icon for license](${licenseURL})
+const generator = (data) => (`# ${data.projectTitle}
+## ${data.projectDescription}
+## ${data.projectLicense}
+${data.gitUsername}
+${data.email}`)
 
-// function to initialize program
-function init() {
+questions().then((data) => fs.writeFileSync(`${data.projectTitle}` + '.md', generator(data)))
 
-}
+.then(() => console.log("MD worked"))
 
-// function call to initialize program
-init();
+.catch((err) => console.log(err))
