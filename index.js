2b9d8df63a6e475133e3583f697e70be64975889
Hever Calles <hevercalles@gmail.com>
2020-11-15 11:46:53 -0500

    finalizing

--- index.js
+++ index.js
@@ -1,46 +1,118 @@
-const fs = require('fs')
-const inquirer = require('inquirer')
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // array of questions for user
-const questions = () => inquirer.prompt ([
+const questions = () =>
+  inquirer.prompt([
     {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'What is the name of your project?'
-    },    
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your project?",
+    },
     {
-        type: 'input',
-        name: 'projectDescription',
-        message: 'Provide description of your project.'
-    },    
+      type: "input",
+      name: "projectDescription",
+      message: "Provide description of your project:",
+    },
     {
-        type: 'list',
-        name: 'projectLicense',
-        message: 'What license are you using?',
-        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
-    },   
+      type: "input",
+      name: "projectInstall",
+      message: "Provide Installation Steps:",
+    },
     {
-        type: 'input',
-        name: 'gitUsername',
-        message: 'Enter your GitHub Username.'
-    },  
+      type: "input",
+      name: "projectUsage",
+      message: "Provide usage details:",
+    },
     {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email.'
-    },   
-])
+      type: "input",
+      name: "projectTest",
+      message: "Provide testing instructions:",
+    },
+    {
+      type: "input",
+      name: "projectContributors",
+      message: "Name project contributors:",
+    },
+    {
+      type: "list",
+      name: "projectLicense",
+      message: "What license are you using?",
+      choices: [
+        "None",
+        "Apache License 2.0",
+        'BSD 3-Clause "New" or "Revised" license',
+        'BSD 2-Clause "Simplified" or "FreeBSD" license',
+        "GNU General Public License v3 (GPL v3)",
+        'GNU Library or "Lesser" General Public License v3 (LGPL v3)',
+        "MIT license",
+        "Mozilla Public License 2.0",
+        "Public Domain Dedication and License (PDDL)",
+        "Eclipse Public License 1.0",
+      ],
+    },
+    {
+      type: "input",
+      name: "gitUsername",
+      message: "Enter your GitHub Username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email: ",
+    },
+  ]);
 
-//![icon for license](${licenseURL})
-const generator = (data) => (`# ${data.projectTitle}
-## ${data.projectDescription}
-## ${data.projectLicense}
-${data.gitUsername}
-${data.email}`)
+const generator = (data) =>
+  `
+  # ${data.projectTitle}
 
-questions().then((data) => fs.writeFileSync(`${data.projectTitle}` + '.md', generator(data)))
+  * [Description](#description)
+  * [Installation Instructions](#installation)
+  * [Usage Information](#usage)
+  * [Test](#test)
+  * [Contributors](#contributors)
+  * [License](#license)
 
-.then(() => console.log("MD worked"))
+  
+  ## Description
 
-.catch((err) => console.log(err))
+  ${data.projectDescription}
+
+  ## Installation Instructions
+
+  ${data.projectInstall}
+
+  ## Usage Information
+
+  ${data.projectUsage}
+
+  ## Test
+
+  ${data.projectTest}
+
+  ## Contributors
+
+  ${data.projectContributors}
+
+  ## License
+
+  ${data.projectLicense}
+
+
+  GitHub Profile: https://github.com/${data.gitUsername}
+
+  Email: ${data.email}
+
+    `
+
+;
+
+questions()
+  .then((data) =>
+    fs.writeFileSync(`${data.projectTitle}` + "README.md", generator(data))
+  )
+
+  .then(() => console.log("MD created"))
+
+  .catch((err) => console.log(err));
